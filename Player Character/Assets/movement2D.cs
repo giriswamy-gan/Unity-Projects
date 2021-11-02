ebddfa1445e09d1e6353669cd0ec5ab5d45a24a6
Hari Ganesan <hariganation@gmail.com>
2021-11-02 20:29:56 +0530

    Version 2

---
+++ Player Character/Assets/movement2D.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+    //Animation 
+    public Animator animator;
+    //Serialize Field initializes value
+    public float speed = 10f;
+    public float jump_force ;
+    bool isGrounded = false; 
+    public Transform isGroundedChecker; 
+    public float checkGroundRadius = 0.1f; 
+    public LayerMask groundLayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+        CheckIfGrounded();
+    }
+    
+    //Flipping the body wrt button Clicked
+    void flip(float x){
+        Vector3 theScale = transform.localScale;
+        if(x!=0){
+            theScale.x = (x>0)? 1:-1;
+        }  
+        transform.localScale = theScale;
+    }
+    //2D Horizontal Movement
+    void Move(){
+        float x = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Horizontal",Mathf.Abs(x));
+        flip(x);
+        float moveBy = x * speed;
+        rb.velocity = new Vector2(moveBy, rb.velocity.y);
+    }
+    //Player Jump
+    void Jump(){
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded){
+            rb.velocity = new Vector2(rb.velocity.x, jump_force);
+            animator.SetBool("Space",true);
+        }
+    }
+    //Checking if on Ground
+    void CheckIfGrounded() { 
+    Collider2D collider = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer); 
+    if (collider != null) { 
+        isGrounded = true;
+    } else { 
+        isGrounded = false;
+    } 
+}
+}
